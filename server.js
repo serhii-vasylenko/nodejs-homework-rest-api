0bdc39357afe924562f74271fca6ce1385e0be07
Jerrssey <fankton@gmail.com>
2023-05-25 22:36:29 +0300

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  'mongodb+srv://jerrssey:YLtZkTy8G4Cf6NRU@cluster0.hgfhos6.mongodb.net/?retryWrites=true&w=majority';
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log('Database connection successful!');
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
