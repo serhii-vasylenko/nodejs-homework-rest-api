730262d9ec6880a86c09d8c2729ee360a9d75a8b
Jerrssey <fankton@gmail.com>
2023-06-03 20:28:21 +0300

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -18,7 +18,7 @@ const authenticate = async (req, res, next) => {
     const user = await User.findById(id);
     req.user = user;
 
-    if (!user || !user.token || user.token !== token) {
+    if (!user.token) {
       next(HttpError(401));
     }
     next();
