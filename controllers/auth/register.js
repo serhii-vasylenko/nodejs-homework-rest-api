c8ac1e5a6d7093c75da815ca988fad3d674e8204
Jerrssey <fankton@gmail.com>
2023-06-04 22:49:32 +0300

    ADDED: gravatar

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,10 +1,12 @@
 const bcrypt = require('bcrypt');
+const gravatar = require('gravatar');
 
 const { HttpError } = require('../../helpers');
 const { User } = require('../../models');
 
 const register = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body;  
+
   const user = await User.findOne({ email });
 
   if (user) {
@@ -12,8 +14,13 @@ const register = async (req, res) => {
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email, { protocol: "https" });
 
-  const newUser = await User.create({ ...req.body, password: hashedPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashedPassword,
+    avatarURL,
+  });
   res.status(201).json(newUser);
 };
 
