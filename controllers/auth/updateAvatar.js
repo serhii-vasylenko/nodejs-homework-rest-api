db17c1537fe6ec4f59f7fa00e56030f8233ad286
Jerrssey <fankton@gmail.com>
2023-06-05 22:37:25 +0300

    minor fix

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -7,15 +7,14 @@ const { User } = require('../../models');
 const avatarDir = path.resolve('public', 'avatars');
 
 const updateAvatar = async (req, res) => {
-  const { _id, avatarURL } = req.user;
-  console.log(req.file);
+  const { _id } = req.user;
   const { path: tmpUpload, mimetype } = req.file;
   const newFileName = `${_id.toString()}_avatar.${mimetype.split('/')[1]}`;
   const resultUpload = path.join(avatarDir, newFileName);
-  const avatar = path.join('avatars', newFileName);
+  const avatarURL = path.join('avatars', newFileName);
 
   await fs.rename(tmpUpload, resultUpload);
-  const user = await User.findByIdAndUpdate(_id, {avatarURL: avatar}, { new: true });
+  const user = await User.findByIdAndUpdate(_id, { avatarURL }, { new: true });
   res.status(200).json(user);
 };
 
